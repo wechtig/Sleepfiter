0cf88f65945fd070ac2e48c73cd2134007b96463
wechtits17 <lukas.wechtitsch@edu.fh-joanneum.at>
2022-11-03 14:03:37 +0100

    added auth service

--- src/main/java/at/fh/Sleepfiter/SleepfiterApplication.java
+++ src/main/java/at/fh/Sleepfiter/SleepfiterApplication.java
@@ -1,13 +1,37 @@
 package at.fh.Sleepfiter;
 
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Verb;
+import com.github.scribejava.core.oauth.OAuthService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 public class SleepfiterApplication {
 
+	final static String API_KEY = "238PNK";
+	final static String API_SECRET = "de61c651987739b72aa08aedaf774668";
 	public static void main(String[] args) {
 		SpringApplication.run(SleepfiterApplication.class, args);
-	}
 
+		OAuthService service = new ServiceBuilder(API_KEY)
+				.apiSecret(API_SECRET)
+				.build(FitbitApi20.instance());
+
+		OAuthRequest oAuthRequest = new OAuthRequest(Verb.GET, "/");
+
+		try {
+			var response = service.execute(oAuthRequest);
+			System.out.println("res: " + response.getBody());
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		} catch (ExecutionException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
